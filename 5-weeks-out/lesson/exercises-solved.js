4ad5b94438100f80dcb28d067606d85d8c355450
Meg Ducharme <ducharme.meg@gmail.com>
2018-05-31 20:45:52 -0500

    updated notes

--- 5-weeks-out/lesson/exercises-solved.js
+++ 5-weeks-out/lesson/exercises-solved.js
@@ -11,12 +11,13 @@ console.log("Hello, ALL!");
 var firstName = "Meg";
 
 
-//when you declare a variable and assign a value to it, JavaScript will read the right side of the equation, evaluate the value, and store the value in the left side (in the variable name). That variable name now represents that value.
+//when you declare a variable and assign a value to it, JavaScript will read the right side of the equation,
+//evaluate the value, and store the value in the left side (in the variable name). That variable name now //represents that value.
 var num1 = 3 + 5;//8 is stored in num1
 console.log(num1) //8
 
 
-//co27 is an array. Arrays are like lists and you can access each item in the list with bracket notataion and the index you want. Indexes start at 0. 
+//co27 is an array. Arrays are like lists and you can access each item in the list with bracket notataion //and the index you want. Indexes start at 0. 
 var co27 = ["John", "Grady", "Klaus"];
 var firstPerson = co27[0]; //JavaScript will evaluate what's on the right side of the equation and store it in the left
 console.log(firstPerson); //John
@@ -40,11 +41,11 @@ var cohort27 = {
 
 console.log(cohort27.instructor);//Brenda Long
 console.log(cohort27.students[2]);//Grady
-//we will access data within an object the same way we are used to, with brack notation when we are dealing with an array, and dot notation when we are dealing with an object. Here, we are going into the cohort27 object, then into the students array and then into the meg object
+//we will access data within an object the same way we are used to, with brack notation when we are dealing //with an array, and dot notation when we are dealing with an object. Here, we are going into the cohort27 //object, then into the students array and then into the meg object
 console.log(cohort27.students[0].lastName);//Ducharme
 
 
-//accesssing the DOM from JavaScript and inserting an inspirational message via JavaScript - don't worry about this too much for right now, but know that it can be done. 
+//accesssing the DOM from JavaScript and inserting an inspirational message via JavaScript - don't worry //about this too much for right now, but know that it can be done. 
 var c27 = document.getElementById("c27");
 c27.innerHTML = "You can do it!🐷";
 
@@ -76,18 +77,24 @@ console.log("My name is " + fullName);
 var favoritePlaces = ["Baltimore", "Thailand", "Johnson City", "Crema", "Boston"];
 
 
-//arrays have PROPERTIES on them, just like objects. The length property will tell us how many items are in an array. We access that by doing array.length
+//arrays have PROPERTIES on them, just like objects. The length property will tell us how many items are in //an array. We access that by doing array.length
 console.log(favoritePlaces.length);
 
 
 //loop over the array and print each of the places to the console
-for (var i = 0; i < 5; i++) {
+//a for loop will run the block of code between the curly brackets as many times as the condition in the loop
+//the condition here is i < favoritePlaces.length (which is 5)
+//each time through the loop (starting at 0), i++ means that i will increase by one = i++ is the same as i = i + 1
+//as i increases by one, i < favoritePlaces.length will fail when i reaches 5 because 5 is equal to, not less than 5
+//when the condition fails, the loop will end
+//we also use favoritePlaces[i] in the loop becuase as i increases, it accesses the next index in the array
+for (var i = 0; i < favoritePlaces.length; i++) {
     console.log(i);
     console.log(favoritePlaces[i]);
 }
 
 
-//functions are little bits of code that you want to run over and over and over again. You can pass the function "raw materials" and get out a "finished product." In this example, we are passing the function numbers as raw materials and getting the sum as the finished product.
+//functions are little bits of code that you want to run over and over and over again. You can pass the //function "raw materials" and get out a "finished product." In this example, we are passing the function n//umbers as raw materials and getting the sum as the finished product.
 //when you declare (or define) a function, it will not run until you call or invoke it (line 92)
 //when you return a value from a funtion, it will return to where you called the function (line 92)
 var add = function(num1, num2){
@@ -97,7 +104,7 @@ var add = function(num1, num2){
 var addedNumbers = add(3, 4);
 
 
-//write a function that takes two arguments, a drink type and a coffee shop name. Print the sentence to the console 'Meg wants a latte from Frothy Monkey'
+//write a function that takes two arguments, a drink type and a coffee shop name. Print the sentence to the //console 'Meg wants a latte from Frothy Monkey'
 var getMeCoffee = function(drink, shop){
   console.log("Meg wants a " + drink + " from " + shop);
 };
@@ -150,7 +157,7 @@ getMeCoffee("cortado", "Barista Parlor");//Meg wants a cortado from Barista Parl
 
 
 
-//DO NOT LOOK AT THIS, IT'S FOR FUN TO PRINT TO THE DOM. If you have a lot of programming experince and want to try to figure it out, you can ask us about it 
+//DO NOT LOOK AT THIS, IT'S FOR FUN TO PRINT TO THE DOM. If you have a lot of programming experince and want //to try to figure it out, you can ask us about it 
 ////////Schedule for Tonight -- REMINDER: DONT LOOK AT THIS!
 let topics = ['hangout', 'meet your friends', 'intro to JS', 'loops', 'writing to the DOM'];
 localStorage.setItem('topics', JSON.stringify(topics));
